281729926ecaf1357be059a3f8659a6ce1fb4099
Shahar Kabesa <61517666+shaharkabesa@users.noreply.github.com>
2023-02-13 15:30:28 +0200

    Update verifytoken.js

--- routes/verifytoken.js
+++ routes/verifytoken.js
@@ -4,7 +4,8 @@ module.exports = function(req, res, next) {
   const token = req.header('auth-token');
   if(!token) return res.status(401).JSON({message:'Access Denied'});
   try {
-    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    const verified = jwt.verify(token, `${process.env.TOKEN_SECRET}`);
+
     req.user = verified;
   
     next();
